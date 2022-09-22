c37eb5dae29417e6704866fe4f2ab42cb12ed08d
nizaralydrus <nizarmonzaa@gmail.com>
2022-09-22 14:41:16 -0500

    beginning

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Calculator {
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args){
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Principal: ");
+        int principal = scanner.nextInt();
+
+
+    }
 }
