09d9a5e0664a246d8a6a4d3df0e9bd723495fecb
nizaralydrus <nizarmonzaa@gmail.com>
2022-09-22 15:26:31 -0500

    add some calculation

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,4 @@
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Calculator {
@@ -10,6 +11,20 @@ public class Calculator {
 
         System.out.println("Principal: ");
         int principal = scanner.nextInt();
+
+        System.out.println("Annual Interest Rate: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.println("Period (Years): ");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double mortgage = principal* (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)) / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
+
 
 
     }
